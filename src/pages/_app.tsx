d9053a1938ad994f9472a2d1048b43a886ba1270
takamasa suzuki <midgard10257@gmail.com>
2021-11-14 14:53:00 +0900

    react-bootstrap-table-nextの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.sass'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/Layout'
